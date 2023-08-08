8b98f70d7fafc64b7b40f369a6da2c84ae608807
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-08-08 12:04:06 +0300

    modified:   api/v1/app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,13 +15,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 if getenv("AUTH_TYPE") == "auth":
-    from api.v1.auth.auth import Auth 
+    from api.v1.auth.auth import Auth
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth 
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-    
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -29,11 +28,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def unauthorized(error) -> str:
@@ -63,4 +64,4 @@ def before_request() -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port,debug=True)
+    app.run(host=host, port=port, debug=True)
