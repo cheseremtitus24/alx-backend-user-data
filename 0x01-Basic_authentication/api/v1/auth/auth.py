e9e441d47b1d421f838252cfc4c406d01aa14d6e
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-08-08 13:00:55 +0300

    modified:   api/v1/auth/auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,7 +4,6 @@ Implements the Authentication Base Class
 """
 from typing import List, TypeVar
 from flask import request
-import re
 
 
 class Auth:
@@ -22,13 +21,11 @@ class Auth:
             return False
         if path + '/' in excluded_paths:
             return False
-        pattern = r"^(?:[\w\-_./]+/)*[\w\-_.]+$"
-        match = re.match(pattern, path)
-        if match:
-            for excluded_path in excluded_paths:
-                if re.match(excluded_path, path):
-                    return False
-                return True
+        astericks = [stars[:-1]
+                     for stars in excluded_paths if stars[-1] == '*']
+        for stars in astericks:
+            if path.startswith(stars):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
